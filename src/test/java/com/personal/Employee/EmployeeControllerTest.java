02a3351a565d3691bc482fccdd033f347bc6fb71
Opeoluwa <user@Users-MacBook-Pro.local>
2023-11-18 02:12:55 +0100

    final commit

--- src/test/java/com/personal/Employee/EmployeeControllerTest.java
+++ src/test/java/com/personal/Employee/EmployeeControllerTest.java
@@ -11,10 +11,6 @@ import java.util.List;
 
 import static org.mockito.Mockito.when;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
